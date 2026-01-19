6038f6e32bc2a21826065f34e2456b6ac1fef05c
Kami <josephkiarie561@gmail.com>
2026-01-19 22:27:37 +0300

    creating orders and customers tables

--- SQL/brazil_ecommerce.sql
+++ SQL/brazil_ecommerce.sql
@@ -1,2 +1,10 @@
 create database olist;
 use olist;
+
+create table customers (
+	customer_id varchar(50) primary key,
+    customer_unique_id varchar(50),
+    customer_zip_code_prefix int,
+    customer_city varchar(100),
+    customer_state char(2)
+);
